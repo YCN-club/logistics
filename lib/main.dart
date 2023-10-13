01c77a49585a278e56fe62b8cfb944944cca0770
Abhigyan Tripathi <abhigyantrips@gmail.com>
2023-10-13 23:01:03 +0530

    feat: init login view

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'src/app.dart';
-import 'src/settings/controllers/settings_controller.dart';
-import 'src/settings/services/settings_service.dart';
 
 void main() async {
-  final settingsController = SettingsController(SettingsService());
-  await settingsController.loadSettings();
-
-  runApp(MyApp(settingsController: settingsController));
+  runApp(const MyApp());
 }
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,28 +1,15 @@
 import 'package:flutter/material.dart';
 
-import 'settings/controllers/settings_controller.dart';
-import 'settings/views/settings_view.dart';
+import 'login/views/login_view.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp({
-    super.key,
-    required this.settingsController,
-  });
-
-  final SettingsController settingsController;
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListenableBuilder(
-      listenable: settingsController,
-      builder: (BuildContext context, Widget? child) {
-        return MaterialApp(
-            restorationScopeId: 'app',
-            theme: ThemeData(useMaterial3: true),
-            darkTheme: ThemeData.dark(),
-            themeMode: settingsController.themeMode,
-            home: SettingsView(controller: settingsController));
-      },
-    );
+    return MaterialApp(
+        restorationScopeId: 'app',
+        theme: ThemeData.dark(useMaterial3: true),
+        home: const LoginView());
   }
 }
